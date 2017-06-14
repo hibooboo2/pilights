06b42e58f9164bf8e23629c551f5ce65c66e404d
Pi Jeffrey <wizardofmath+pi@gmail.com>
2017-06-14 20:39:06 +0000

    Blinking lights

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	rpio "github.com/stianeikeland/go-rpio"
+)
+
 func main() {
-	println("fdsfd")
+	if err := rpio.Open(); err != nil {
+		panic(err)
+	}
+	pin := rpio.Pin(26)
+	pin2 := rpio.Pin(11)
+	defer pin.Low()
+	defer pin2.Low()
+	defer rpio.Close()
+
+	pin.Output()
+	pin2.Output()
+	pin2.High()
+	for {
+		pin.Toggle()
+		pin2.Toggle()
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(400)))
+	}
 }
+
